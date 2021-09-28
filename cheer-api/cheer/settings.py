c34c60a767b2543e0a1e433efc980c487b371785
Alex Mathew <alexmathew003@gmail.com>
2021-09-28 23:39:43 +0530

    Create new django app

--- cheer-api/cheer/settings.py
+++ cheer-api/cheer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x+rr-ibkd&(+o8#0m%id6kv0334tahf2-=@qk$62#*tk+_@^a3"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG") == "true" else False
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "twickr",
 ]
 
 MIDDLEWARE = [
