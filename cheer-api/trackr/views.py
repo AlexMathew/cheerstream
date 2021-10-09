8d52b5cea35e4bf647fa65ccff7704f495f40104
Alex Mathew <alexmathew003@gmail.com>
2021-10-09 16:57:50 +0530

    Return static event stats in view

--- cheer-api/trackr/views.py
+++ cheer-api/trackr/views.py
@@ -1,8 +1,23 @@
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
 from django.views import View
+from helpers.instances import redis
+
+from .constants import TRACKR_PREFIX
 
 
 class RealtimeStatsView(View):
     def get(self, request: HttpRequest, *args, **kwargs):
-        return JsonResponse({})
+        return JsonResponse(
+            {
+                "events": [
+                    {
+                        "event": event.decode().split(TRACKR_PREFIX)[-1],
+                        "created_at": redis.get(event).decode(),
+                        "count": int(redis.get(f"count_{event.decode()}") or 0),
+                        "max": int(redis.get(f"max_{event.decode()}") or 0),
+                    }
+                    for event in redis.r.keys(pattern=f"{TRACKR_PREFIX}*")
+                ]
+            }
+        )
