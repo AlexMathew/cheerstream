684a96c7367941b90dff023bc57b9f977e018607
Alex Mathew <alexmathew003@gmail.com>
2021-10-19 01:50:04 +0530

    Add header to landing page

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
-  return <div className="App">App</div>;
+  return <Header />;
 };
 
 export default App;
---
+++ landing/src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header className="sticky top-0 z-30 h-16 bg-skin-fill bg-opacity-60 backdrop-filter firefox:bg-opacity-90 backdrop-blur-xl px-8 py-8">
+      <div className="flex items-center justify-between h-full md:mx-auto">
+        <div className="cursor-pointer flex items-center justify-between">
+          twickr
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-36">
+            <a
+              href="https://chrome.google.com/webstore/detail/twickr-for-hotstar/ohjenobipjkabhgcfhglplhaeagjlgdn/"
+              target="_blank"
+              rel="noopener noreferrer"
+              draggable="false"
+              className="btn hover:scale-98 active:scale-95 w-36 text-base md:grid place-content-center hidden noselect"
+            >
+              Add to Chrome
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
