d7a56eec33169663a2bc27d7cd389d897eeb7319
Alex Mathew <alexmathew003@gmail.com>
2021-10-19 03:02:18 +0530

    Add intro section

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import { Intro } from "./components/Intro";
 
 const App: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className="max-w-7xl mx-auto">
+        <Intro />
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ landing/src/components/Intro.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface IntroProps {}
+
+export const Intro: React.FC<IntroProps> = () => {
+  return (
+    <div className="px-7 lg:px-0 m-auto mt-20 text-center">
+      <h1 className="mx-auto font-bold text-3xl md:text-5xl">
+        Live sports and tweets — all in one screen
+      </h1>
+      <p className="max-w-6xl sm:max-w-screen-sm mx-auto font-normal opacity-60 mt-7 text-lg md:text-2xl">
+        Twickr lets you live stream tweets while watching live sports on Hotstar
+      </p>
+      <a
+        href="https://chrome.google.com/webstore/detail/twickr-for-hotstar/ohjenobipjkabhgcfhglplhaeagjlgdn/"
+        target="_blank"
+        rel="noopener noreferrer"
+        draggable="false"
+        className="inline-block text-white mt-8 btn text-base hover:scale-98 active:scale-95 noselect w-36"
+      >
+        Add to Chrome
+      </a>
+    </div>
+  );
+};
