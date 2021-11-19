8bdb308e4d3110ea1cf335c7570fc5b3358528d7
Alex Mathew <alexmathew003@gmail.com>
2021-11-19 19:37:29 +0530

    I'm an idiot 🤦‍♂️

--- cheer-extension/src/constants.ts
+++ cheer-extension/src/constants.ts
@@ -13,11 +13,18 @@ export const HOTSTAR_DEFAULT_SPORT = 'sports';
 export const EVENTS = {
   IPL: 'indian-premier-league',
   T20_WC: 'icc-mens-t20-world-cup',
+  NZ_TOUR_OF_INDIA: 'new-zealand-tour-of-india-202122',
+  SYED_MUSHTAQ_ALI_TROPHY: 'syed-mushtaq-ali-trophy',
 };
 
 export const SUPPORTED_SPORTS = [SPORTS.FOOTBALL, SPORTS.F1];
 export const FALLBACK_SUPPORTED_SPORTS = [...SUPPORTED_SPORTS, SPORTS.CRICKET];
-export const SUPPORTED_EVENTS = [EVENTS.IPL, EVENTS.T20_WC];
+export const SUPPORTED_EVENTS = [
+  EVENTS.IPL,
+  EVENTS.T20_WC,
+  EVENTS.NZ_TOUR_OF_INDIA,
+  EVENTS.SYED_MUSHTAQ_ALI_TROPHY,
+];
 
 export const TWEETS_LOCAL_STORAGE_PREFIX = 'twickr_tweets_';
 
