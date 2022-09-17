55ed82898b868b573c18ff05e90d3ae34959299f
Alex Mathew <alexmathew003@gmail.com>
2022-09-18 04:42:55 +0530

    PROMPTHERO: Add csrf exempt

--- cheer-api/twickr/views.py
+++ cheer-api/twickr/views.py
@@ -51,6 +51,11 @@ class MostRecentTweetView(View):
         )
 
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+@method_decorator(csrf_exempt, name="dispatch")
 class PromptHeroGumroadPingView(View):
     def post(self, request: HttpRequest, *args, **kwargs):
         import json
